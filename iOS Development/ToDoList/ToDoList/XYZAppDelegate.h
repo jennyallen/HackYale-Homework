ba8ba2d04097c7e4e63a76dfcbab408c05e7ee0d
Jenny Allen <jennifer.allen@yale.edu>
2014-08-23 10:42:27 -0400

    Rails, database creation

---
+++ iOS Development/ToDoList/ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Jenny Allen on 7/30/14.
+//  Copyright (c) 2014 The J Crew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS Development/ToDoList/ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Jenny Allen on 7/30/14.
+//  Copyright (c) 2014 The J Crew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
